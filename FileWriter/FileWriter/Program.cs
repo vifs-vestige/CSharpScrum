5e33e5a632920595485dbf2485a61b5adfb5314e
JackGilliam1 <Jack-Gilliam@hotmail.com>
2012-08-20 18:02:13 -0700

    Separated parts of the program

--- FileWriter/FileWriter/Program.cs
+++ FileWriter/FileWriter/Program.cs
@@ -21,29 +21,15 @@ namespace FileWriter
             try
             {
                 String rootDirectory = Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.System));
+
                 String path = rootDirectory + "1.txt";
-                FileStream fileStream;
-                /* if file at the path above doesn't exist it, creates it then opens it, otherwise it just opens it*/
-                if (!File.Exists(path))
-                {
-                    Console.WriteLine("Creating file: " + path);
-                    fileStream = File.Create(path);
-                }
-                else
-                {
-                    Console.WriteLine("Opening file: " + path);
-                    fileStream = File.OpenWrite(path);
-                }
-                String toWrite = "Done writing to file! fodslighroiufhgdsoughfdoiugnhiugfneoakndx";
-                Console.WriteLine("Writing " + toWrite + " to file: " + path);
-                /* writes stuff to the opened file */
-                StreamWriter writer = new StreamWriter(fileStream);
-                writer.WriteLine(toWrite);
-                /* makes sure its sent to be written */
-                writer.Flush();
-                /* closes the file */
-                writer.Close();
-                fileStream.Close();
+                FileStream fileStream = OpenFile(path);
+                String toWrite = "I Wrote Stuff fodslighroiufhgdsoughfdoiugnhiugfneoakndx";
+                Console.WriteLine("Writing: " + toWrite + ", to file: " + path);
+
+                Write(fileStream, toWrite);
+                CloseStream(fileStream);
+
                 /* and done */
                 Console.WriteLine("Done writing to file: " + path);
                 Console.WriteLine("Closing file: " + path);
@@ -52,6 +38,63 @@ namespace FileWriter
             {
                 Console.WriteLine(e.Message);
             }
+        }
+
+        public FileStream OpenFile(string path)
+        {
+            FileStream fileStream;
+            /* if file at the path above doesn't exist it, creates it then opens it, otherwise it just opens it*/
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Creating file: " + path);
+                fileStream = File.Create(path);
+            }
+            else
+            {
+                Console.WriteLine("Opening file: " + path);
+                fileStream = File.OpenWrite(path);
+            }
+            return fileStream;
+        }
+
+        public void Write(FileStream fileStream, string toWrite)
+        {
+            /* writes stuff to the opened file */
+            using (StreamWriter writer = new StreamWriter(fileStream))
+            {
+                writer.WriteLine(toWrite);
+                /* makes sure its sent to be written */
+                writer.Flush();
+            }
+        }
+
+        public string Read(FileStream fileStream)
+        {
+            string data = null;
+            if (fileStream != null)
+            {
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    data = reader.ReadToEnd();
+                }
+            }
+            return data;
+        }
+
+        public void Delete(FileStream fileStream)
+        {
+
+        }
+
+        public void Update(FileStream fileStream, string newData)
+        {
+
+        }
+
+        public void CloseStream(FileStream fileStream)
+        {
+            if (fileStream != null)
+                fileStream.Close();
         }
     }
 }
